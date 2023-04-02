2f40d2cd114172dbed07c01169545238ca489585
NPare <NPare@EN1710346>
2023-04-02 18:56:03 -0300

    Add project files.

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Permita el ingreso de frases, finalizando cuando el usuario ingresa “fin”.
+
+var frase = "";
+
+do
+{
+    Console.WriteLine("Por favor, ingrese una frase. Si ingresa la palabra fin, el programa finalizará.");
+    frase = Console.ReadLine();
+} while (frase.ToUpper() != "FIN");
